10f1ec11a7dc5e545fa536599df37b885d6fbdcf
Shawn Presser <shawnpresser@gmail.com>
2018-05-16 16:11:42 -0500

    Update deploy.cmd to use webmr

--- deploy.cmd
+++ deploy.cmd
@@ -3,4 +3,4 @@ robocopy /MIR build deploy\build
 robocopy /MIR examples\js deploy\js
 robocopy /MIR examples\lightsaber deploy\lightsaber
 copy examples\webgl_demo_lightsaber.html deploy\index.html
-now deploy\
+webmr url deploy
