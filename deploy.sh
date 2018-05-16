6ade5b8c3ef2bd1477f69ff33aaef0289dd19e6e
Shawn Presser <shawnpresser@gmail.com>
2018-05-16 16:14:02 -0500

    Fix deploy.sh paths

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ set -x
 set -e
 mkdir -p deploy
 rsync -Pa build/ deploy/build/
-rsync -Pa examples/js deploy/js
-rsync -Pa examples/lightsaber deploy/lightsaber
+rsync -Pa examples/js/ deploy/js/
+rsync -Pa examples/lightsaber/ deploy/lightsaber/
 cp examples/webgl_demo_lightsaber.html deploy/index.html
 webmr url deploy
 
